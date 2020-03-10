d03b1857372e25e0112ef2a7ce7475cee38e6eb7
Darren Cedrone <dcedrone@Darrens-Air.home>
2020-03-09 20:22:06 -0400

    added tests

---
+++ tests/strictEqual.test.js
@@ -0,0 +1,41 @@
+const describe = require('mocha').describe
+const it = require('mocha').it
+const expect = require('chai').expect
+const strictEqual = require('../strictEqual')
+describe('comparison', () => {
+  describe('strictEqual', () => {
+    it('returns true if both numbers are equal', () => {
+      const a = 5
+      const b = 5
+      const returnTrueNumber = strictEqual(a, b)
+      expect(returnTrueNumber).to.equal(true)
+    })
+    it('returns false if both inputs are not same data type', () => {
+      const a = 5
+      const b = '5'
+      const returnFalseNumber = strictEqual(a, b)
+      expect(returnFalseNumber).to.equal(false)
+    })
+    it('returns true if both strings are the same value', () => {
+      const a = 'a'
+      const b = 'a'
+      const returnTrueString = strictEqual(a, b)
+      expect(returnTrueString).to.equal(true)
+    })
+    it('returns false if both strings are different values', () => {
+      const a = 'a'
+      const b = 'b'
+      const returnFalseString = strictEqual(a, b)
+      expect(returnFalseString).to.equal(false)
+    })
+
+    // 
+    // const returnTrueString = strictEqual('a', 'a')
+    // const returnFalseString = strictEqual('a', 'b')
+
+
+    // 
+    // expect(returnTrueString).to.equal(true)
+    // expect(returnFalseString).to.equal(false)
+  })
+})
